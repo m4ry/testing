2f5aca7d19fcb9af0296d347165ca75edf70142c
Marlo Longley <maryelongley@gmail.com>
2011-07-28 15:55:04 -0700

    deploy settings changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,29 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
-
-ssh_options[:forward_agent] = true
-
-
-
+set :application, "m.xn--ycee"
+set :repository,  "git@github.com:m4ry/testing.git"
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :deploy_via, :remote_cache
+set :copy_compression, :bz2
+set :rails_env, 'production'
+set :deploy_to, "/home/webghostingshell/#{application}"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "#{application}"                          # Your HTTP server, Apache/etc
+role :app, "#{application}"                          # This may be the same as your `Web` server
+role :db,  "#{application}", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+
+
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end

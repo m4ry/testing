23f279ab6b05f25577303bd96dc466807b55b63f
Marlo Longley <maryelongley@gmail.com>
2011-07-28 23:03:24 -0700

    hm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :user, 'm4shell'  # Your dreamhost account's username
-set :domain, 'www.x.xn--ycee.net'  # Dreamhost servername where your account is located 
+set :domain, 'x.xn--ycee.net'  # Dreamhost servername where your account is located 
 set :project, 'testing'  # Your application as its called in the repository
-set :application, 'www.x.xn--ycee.net'  # Your app's location (domain or sub-domain name as setup in panel)
+set :application, 'x.xn--ycee.net'  # Your app's location (domain or sub-domain name as setup in panel)
 set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
 
 default_run_options[:pty] = true  # Must be set for the password prompt from git to work
@@ -16,12 +16,12 @@ role :app, domain
 role :db,  domain, :primary => true
 
 # deploy config
+set :deploy_to, applicationdir
 set :deploy_via, :export
 
 # additional settings
 default_run_options[:pty] = true  # Forgo errors when deploying from windows
 #ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
-set :chmod755, "app config db lib public vendor script script/* public/disp*"
 set :use_sudo, false
 # If you are using Passenger mod_rails uncomment this:
  namespace :deploy do

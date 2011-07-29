0da7fc01ac4d243076dd64a967d3e4c8ce482d63
Marlo Longley <maryelongley@gmail.com>
2011-07-28 23:49:08 -0700

    pty

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
+default_run_options[:pty] = true  
+
 set :user, 'm4shell'  # Your dreamhost account's username
 set :domain, 'x.xn--ycee.net'  # Dreamhost servername where your account is located 
 set :project, 'testing'  # Your application as its called in the repository
 set :application, 'x.xn--ycee.net'  # Your app's location (domain or sub-domain name as setup in panel)
 set :applicationdir, "/home/#{user}/#{application}"  # The standard Dreamhost setup
 
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
 set :repository, "git@github.com:m4ry/testing.git"  # Your clone URL
 set :scm, "git"
 
 
+ssh_options[:auth_methods]
 
 # roles (servers)
 role :web, domain
@@ -20,7 +22,6 @@ set :deploy_to, applicationdir
 set :deploy_via, :export
 
 # additional settings
-default_run_options[:pty] = true  # Forgo errors when deploying from windows
 #ssh_options[:keys] = %w(/Path/To/id_rsa)            # If you are using ssh_keys
 set :use_sudo, false
 # If you are using Passenger mod_rails uncomment this:
